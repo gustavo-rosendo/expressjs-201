ad42db8ae0bd89c3a4fcb5e08c23dd2eebdf79c5
Gustavo M. R. Santos <gustavo.rosendo@b-plat.net>
2022-02-10 18:55:25 -0300

    - explore deeper the EJS features

--- expressRendering.js
+++ expressRendering.js
@@ -9,6 +9,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: '5mb' }));
 
+// Choose which view engine to use: EJS, Handlebars or Pug
 app.set('view engine', 'ejs');
 // app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
@@ -25,8 +26,24 @@ app.set('views', path.join(__dirname, 'views'));
 // 4. Express takes the HTML/JS/CSS and combines it with the data sent (possibly dynamic data)
 // 5. The final compiled result will be an HTML/JS/CSS file with all the data in the right format for the browser to read
 
+function validateUser(req, res, next) {
+  res.locals.validated = true;
+  next();
+}
+
+app.use(validateUser);
+
 app.get('/', (req, res, next) => {
-  res.render('index');
+  // the data in the 2nd arg is going to be appended to res.locals
+  res.render('index', {
+    msg: "Success!",
+    msg2: "Failure!",
+    html: "<p><i>Parsing and rendering html tags here...</i></p>"
+  });
 });
+
+app.get('/about', (req, res, next) => {
+  res.render('about', {});
+})
 
 app.listen(3000);
